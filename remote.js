8b518f4d47fb064a98dd1d43a5b84c98fc7d7449
kirilkalchev <kiril.kalchev@ecl-labs.org>
2015-06-04 21:18:54 +0300

    added initail remote resrver directory initialization

--- remote.js
+++ remote.js
@@ -1,7 +1,10 @@
 var http = require('http');
+var fs = require('fs-extra');
 
-address=process.argv[process.argv.length - 2];
-port=process.argv[process.argv.length - 1];
+address=process.argv[process.argv.length - 4];
+port=process.argv[process.argv.length - 3];
+sharedDir=process.argv[process.argv.length -2];
+destDir=process.argv[process.argv.length - 1];
 
 var eventDispatch = {
   'add': function(event, file, sha1) {
@@ -24,27 +27,40 @@ var eventDispatch = {
   },
 };
 
-http.createServer(function(request,response){
-  var req = "";
-
-  request.on('data', function(chunk) {
-    req+=chunk
-  });
-
-  request.on('end', function() {
-    var reqObj = JSON.parse(req);
-    var dispatchKey = reqObj["event"];
-
-    if (!(dispatchKey in eventDispatch)) {
-      dispatchKey = 'default';
+//Clean destDirecotry first
+fs.remove(destDir, function(err) {
+  if (err) {
+    console.log(err);
+    return 1;
+  }
+  fs.copy(sharedDir, destDir, function(err) {
+    if (err) {
+      console.log(err);
+      return 1;
     }
+    http.createServer(function(request,response){
+      var req = "";
 
-    eventDispatch[dispatchKey](reqObj['event'], reqObj['file'], reqObj['checkSum']);
+      request.on('data', function(chunk) {
+        req+=chunk
+      });
+
+      request.on('end', function() {
+        var reqObj = JSON.parse(req);
+        var dispatchKey = reqObj["event"];
+
+        if (!(dispatchKey in eventDispatch)) {
+          dispatchKey = 'default';
+        }
+
+        eventDispatch[dispatchKey](reqObj['event'], reqObj['file'], reqObj['checkSum']);
+      });
+
+      response.writeHead(200, {'Content-Type': 'application/json'});
+      response.write(JSON.stringify({code: 200, message: 'Hello'}));
+      response.end();
+    }).listen(port, address);
+
+    console.log("Server is listening to http://" + address + ":" + port);
   });
-
-  response.writeHead(200, {'Content-Type': 'application/json'});
-  response.write(JSON.stringify({code: 200, message: 'Hello'}));
-  response.end();
-}).listen(port, address);
-
-console.log("Server is listening to http://" + address + ":" + port);
+})
