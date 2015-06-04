c2549a9160110243506c7d2250837d4f9452737a
kirilkalchev <kiril.kalchev@ecl-labs.org>
2015-06-04 23:17:24 +0300

    added and change event handlers

--- remote.js
+++ remote.js
@@ -1,23 +1,60 @@
 var http = require('http');
 var fs = require('fs-extra');
+var utils= require('./utils');
 
 address=process.argv[process.argv.length - 4];
 port=process.argv[process.argv.length - 3];
 sharedDir=process.argv[process.argv.length -2];
 destDir=process.argv[process.argv.length - 1];
 
+var copyFile = function(file, sha1) {
+  var src = sharedDir + '/' + file;
+  utils.calcHash(src, function(err, calcHash) {
+    if (err) {
+      console.log('[-] Unable to calculate checksum ' + JSON.stringify(err));
+      return;
+    }
+    if (sha1 != calcHash) {
+      console.log('[-] File Version Missmatch');
+    }
+    var dst = destDir + '/' + file;
+    console.log('[.] Coping File ' + src + ' to ' + dst);
+    fs.copy(src, dst, function(err) {
+      if (err) {
+        console.log('[-] ' + err);
+      } else {
+        console.log('[+] File ' + src + ' copied to ' + dst);
+      }
+    });
+  });
+}
+
+var mkDir = function(dir) {
+  var dst = destDir + '/' + dir;
+  fs.mkdirs(dst, function(err) {
+    if (err) {
+      console.log('[-] Unable to create ' + dst + ' directory');
+    } else {
+      console.log('[+] Directory ' + dst + ' created');
+    }
+  });
+}
+
 var eventDispatch = {
   'add': function(event, file, sha1) {
-    console.log("Added file " + file + " with checksum " + sha1);
+    console.log('[.] add event received for ' + sharedDir + '/' + file );
+    copyFile(file, sha1);
   },
   'unlink': function(event, file, sha1) {
     console.log("Unlinked file " + file);
   },
   'change': function(event, file, sha1) {
-    console.log("Changed file " + file + " with checksum " + sha1);
+    console.log('[.] change event received for ' + sharedDir + '/' + file);
+    copyFile(file, sha1);
   },
   'addDir': function(event, dir, sha1) {
-    console.log("Created directory " + dir);
+    console.log('addDir event received for  ' + sharedDir + '/' + dir);
+    mkDir(dir);
   },
   'unlinkDir': function(event, dir, sha1) {
     console.log("Unlinked directory " + dir);
